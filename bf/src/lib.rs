5b66d013760b47e632dc422b989d451c79399d88
ThreadedStream <frankflipper3@gmail.com>
2022-02-23 23:50:39 +0300

    finished brainfuck interpreter

--- bf/src/lib.rs
+++ bf/src/lib.rs
@@ -1,7 +1,8 @@
+// yet another brainfuck interpreter, blah-blah-blah
 
 fn main() {
 	unsafe {
-		bf::interpret("<<<<>>>>");
+		bf::interpret("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
 	}
 }
 
@@ -9,47 +10,46 @@ mod bf {
 	use std::alloc::{self, Layout};
 	use std::io::{self, Read};
 	use std::slice;
+	use libc::getchar;
 
 	pub unsafe fn interpret(program: &str) {
-		let relevant_layout = Layout::new::<u8>();
-		let mut ptr = alloc::alloc(relevant_layout);
+		let relevant_layout = Layout::new::<char>();
+		let mut original_ptr = alloc::alloc(relevant_layout);
+		let mut ptr = original_ptr;
 		let mut ip: usize = 0;
-		let mut last_jmp_ip: usize = 0;
+		let mut last_jmp_ip: usize = usize::MAX;
+		let chs: Vec<char> = program.chars().collect();
 		loop {
+			if ip == chs.len() {
+				break;
+			}
 			// fetch instruction
 			let c = chs[ip];
 			match c {
-				'>' => ptr = ptr.offset(1),
-				'<' => ptr = ptr.offset(-1),
-				'+' => *ptr += 1,
-				'-' => *ptr -= 1,
-				'.' => print!("{}", *ptr),
-				',' => *ptr = std::io::stdin().bytes().next().and_then(|result| result.ok()).unwrap(),
+				'>' => { ptr = ptr.offset(1); ip += 1 },
+				'<' => { ptr = ptr.offset(-1); ip += 1} ,
+				'+' => {*ptr += 1; ip += 1 },
+				'-' => { *ptr -= 1; ip += 1},
+				'.' => { print!("{}", *ptr as char); ip += 1},
+				',' => { *ptr = getchar() as u8; ip += 1 },
 				'[' => {
 					last_jmp_ip = ip;
-					ip+=1;
+					ip += 1
 				},
+				']' => {
+					if *ptr > 0 && last_jmp_ip != usize::MAX {
+						ip = last_jmp_ip;
+						ip += 1;
+						continue;
+					}
+					last_jmp_ip = usize::MAX;
+					ip += 1;
+				}
 				_ => panic!("unknown token {}!!!", c),
 			}
 		}
-		// for c in program.chars() {
-		// 	match c {
-		// 		'>' => ptr = ptr.offset(1),
-		// 		'<' => ptr = ptr.offset(-1),
-		// 		'+' => *ptr += 1,
-		// 		'-' => *ptr -= 1,
-		// 		'.' => print!("{}", *ptr),
-		// 		',' => *ptr = std::io::stdin().bytes().next().and_then(|result| result.ok()).unwrap(),
-		// 		'[' => {
-		// 			loop {
-						
-		// 			}
-		// 		}
-		// 		_ => panic!("unknown token {}!!!", c),
-		// 	}
-		// }
 
-		alloc::dealloc(ptr, relevant_layout);
+		alloc::dealloc(original_ptr, relevant_layout);
 	}
 
 	pub unsafe fn execute_while_block(program: &str, ptr: &mut *mut u8) {
